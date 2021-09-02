381e05e09a08b3805edb6d0367fb79834e44900b
bronzeraf <rafail.brouzos@workable.com>
2021-09-02 18:29:14 +0300

    Homepage components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,70 @@
 import React, {Component} from 'react'
 import './Home.css';
 import SearchBox from './SearchBox';
-
+import Spinner from '../../utility/Spinner/Spinner';
+import Cities from '../../utility/City/Cities';
+import Activities from '../../utility/Activity/Activities';
+import axios from 'axios';
 
 class Home extends Component{
+    state = {
+        cities: [],
+        europeCities: {},
+        asiaCities: {},
+        exoticCities: {},
+        activities: [],
+        recVenues: {}
+    }
+
+
+    async componentDidMount(){
+        const citiesUrl = `${window.apiHost}/cities/recommended`;
+        const europeCitiesUrl = `${window.apiHost}/cities/europe`;
+        const asiaCitiesUrl = `${window.apiHost}/cities/asia`;
+        const exoticCitiesUrl = `${window.apiHost}/cities/exotic`;
+
+        const citiesPromises = [];
+
+        citiesPromises.push(axios.get(citiesUrl));
+        citiesPromises.push(axios.get(europeCitiesUrl));
+        citiesPromises.push(axios.get(asiaCitiesUrl));
+        citiesPromises.push(axios.get(exoticCitiesUrl));
+
+        Promise.all(citiesPromises).then((data)=>{
+            const recommendedCities = data[0].data;
+            const europeCities = data[1].data;
+            const asiaCities = data[2].data;
+            const exoticCities = data[3].data;
+            
+            this.setState({
+                cities: recommendedCities,
+                europeCities,
+                asiaCities,
+                exoticCities,
+            });
+        })
+
+        const activitiesUrl = `${window.apiHost}/activities/today`;
+        const activities = await axios(activitiesUrl);
+        this.setState({
+            activities: activities.data,
+        });
+
+        const recVenuesUrl = `${window.apiHost}/venues/recommended`;
+        const venues = await axios(recVenuesUrl);
+        this.setState({
+            recVenues: venues.data,
+        })
+    }
+
     render(){
-        return(
+        console.log(this.state.cities.length)
+
+        if(this.state.cities.length === 0){
+            return(<Spinner />)
+        }
+
+        return(<>
             <div className="container-fluid">
                 <div className="row">
                     <div className="home col s12">
@@ -15,6 +74,29 @@ class Home extends Component{
                     </div>
                 </div>
             </div>
+            <div className="container-fluid lower-fold">
+                <div className="row">
+                    <div className="col s12">
+                        <Cities cities={this.state.cities} header="Recommended Cities For You" />
+                    </div>
+        
+                    <div className="col s12">
+                        <Cities cities={this.state.europeCities.cities} header={this.state.europeCities.header} />
+                    </div>
+
+                    <div className="col s12">
+                        <Cities cities={this.state.asiaCities.cities} header={this.state.asiaCities.header} />
+                    </div>
+
+                    <div className="col s12">
+                        <Cities cities={this.state.exoticCities.cities} header={this.state.exoticCities.header} />
+                    </div>
+
+
+
+                </div>
+            </div>
+        </>
         )
     }
 }
